701dfb026aa96655c81892f976994e4a83ddf56f
Alperen Uzun <alperenuzun54@gmail.com>
2020-09-01 23:06:18 +0300

    Updates

---
+++ precache-manifest.283e52c5bb6f91fe7281bafd4b14238e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a65c089231d10ddca7de161f4f5b324",
+    "url": "/basic-virtual-tryon-glasses/index.html"
+  },
+  {
+    "revision": "03e62d6900e9cb060b48",
+    "url": "/basic-virtual-tryon-glasses/static/css/main.817f4119.chunk.css"
+  },
+  {
+    "revision": "4a03eecdd2fdf2fbcf2a",
+    "url": "/basic-virtual-tryon-glasses/static/js/2.817a2c40.chunk.js"
+  },
+  {
+    "revision": "09187598adaca7681bdff78be72e5383",
+    "url": "/basic-virtual-tryon-glasses/static/js/2.817a2c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e62d6900e9cb060b48",
+    "url": "/basic-virtual-tryon-glasses/static/js/main.b1a4618c.chunk.js"
+  },
+  {
+    "revision": "7ed5090f27a67b5787e3",
+    "url": "/basic-virtual-tryon-glasses/static/js/runtime-main.549065be.js"
+  }
+]);
